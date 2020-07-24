cac781b1536b3f549996bea0d71b5f6f066ac231
D4C-lolu <dareolumideisfly@gmail.com>
2020-07-24 23:01:14 +0100

    Updates

---
+++ precache-manifest.d022f8c5accaa1b79456d1ce91bc8b70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e54486e3189f554060dbca7ba28cff2",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "7886fff010b6442463af",
+    "url": "/Tetris/static/css/main.03eed875.chunk.css"
+  },
+  {
+    "revision": "f313df63810cd6ff7d2b",
+    "url": "/Tetris/static/js/2.3be24114.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tetris/static/js/2.3be24114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7886fff010b6442463af",
+    "url": "/Tetris/static/js/main.94369862.chunk.js"
+  },
+  {
+    "revision": "492b925ece97fef24a34",
+    "url": "/Tetris/static/js/runtime-main.d0a4f600.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris/static/media/bg.2810fe6b.png"
+  }
+]);
